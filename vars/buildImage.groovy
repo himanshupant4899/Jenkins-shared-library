443b072df8e7e0d50569ace8323e3a7ed814f880
Himanshu Pant <himanshupant4899@gmail.com>
2022-09-19 12:40:31 +0530

    Extracting logic to groovy class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(){
-    echo "Building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo',passwordVariable: 'PASS',usernameVariable: 'USER')]){
-    sh 'docker build -t himanshupant4899/demo-app:jma-1.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push himanshupant4899/demo-app:jma-1.0'
+def call(String imageName){
+    return new Docker(this).buildDockerImage(imageName)
 }
-}
+
+// Note: Use "" when using variables in string
+
